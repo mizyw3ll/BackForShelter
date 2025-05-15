1e541067f89bf5c49d49808fd2f95cf7f3da425b
kirill12342152 <sanik778345@mail.ru>
2025-05-15 19:44:06 +0300

    Добавление миграций

---
+++ AnimalShelter/Migrations/20250515164233_FirstMigration.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelter.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnimalStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimalViews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalViews", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeePosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeePosts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimalBreeds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnimalViewId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalBreeds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnimalBreeds_AnimalViews_AnimalViewId",
+                        column: x => x.AnimalViewId,
+                        principalTable: "AnimalViews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    Patronymic = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    RoleUserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_RoleUsers_RoleUserId",
+                        column: x => x.RoleUserId,
+                        principalTable: "RoleUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnimalViewId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsMale = table.Column<bool>(type: "boolean", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    AnimalBreedId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DistinctiveFeatures = table.Column<string>(type: "text", nullable: false),
+                    Weight = table.Column<decimal>(type: "numeric", nullable: false),
+                    Height = table.Column<decimal>(type: "numeric", nullable: false),
+                    Photos = table.Column<string>(type: "text", nullable: false),
+                    AnimalStatusId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animals_AnimalBreeds_AnimalBreedId",
+                        column: x => x.AnimalBreedId,
+                        principalTable: "AnimalBreeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Animals_AnimalStatuses_AnimalStatusId",
+                        column: x => x.AnimalStatusId,
+                        principalTable: "AnimalStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Animals_AnimalViews_AnimalViewId",
+                        column: x => x.AnimalViewId,
+                        principalTable: "AnimalViews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeePostId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HireDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_EmployeePosts_EmployeePostId",
+                        column: x => x.EmployeePostId,
+                        principalTable: "EmployeePosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Employees_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Volunteers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Preferences = table.Column<string>(type: "text", nullable: false),
+                    MoreInformation = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Volunteers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Volunteers_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdoptionalApplications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnimalId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ApplicationDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    LivingConditions = table.Column<string>(type: "text", nullable: false),
+                    AppStatusId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdoptionalApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdoptionalApplications_Animals_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AdoptionalApplications_AppStatuses_AppStatusId",
+                        column: x => x.AppStatusId,
+                        principalTable: "AppStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AdoptionalApplications_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemporaryPlacement",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    VolunteerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnimalId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateAnimalTake = table.Column<DateOnly>(type: "date", nullable: false),
+                    DateAnimalReturn = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemporaryPlacement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TemporaryPlacement_Animals_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TemporaryPlacement_Volunteers_VolunteerId",
+                        column: x => x.VolunteerId,
+                        principalTable: "Volunteers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adoptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AdoptionDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    IsAdoption = table.Column<bool>(type: "boolean", nullable: false),
+                    ContractNumber = table.Column<string>(type: "text", nullable: false),
+                    AdoptionApplicationId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adoptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adoptions_AdoptionalApplications_AdoptionApplicationId",
+                        column: x => x.AdoptionApplicationId,
+                        principalTable: "AdoptionalApplications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Adoptions_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdoptionalApplications_AnimalId",
+                table: "AdoptionalApplications",
+                column: "AnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdoptionalApplications_AppStatusId",
+                table: "AdoptionalApplications",
+                column: "AppStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdoptionalApplications_UserId",
+                table: "AdoptionalApplications",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adoptions_AdoptionApplicationId",
+                table: "Adoptions",
+                column: "AdoptionApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adoptions_EmployeeId",
+                table: "Adoptions",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimalBreeds_AnimalViewId",
+                table: "AnimalBreeds",
+                column: "AnimalViewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_AnimalBreedId",
+                table: "Animals",
+                column: "AnimalBreedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_AnimalStatusId",
+                table: "Animals",
+                column: "AnimalStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_AnimalViewId",
+                table: "Animals",
+                column: "AnimalViewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_EmployeePostId",
+                table: "Employees",
+                column: "EmployeePostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_UserId",
+                table: "Employees",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemporaryPlacement_AnimalId",
+                table: "TemporaryPlacement",
+                column: "AnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemporaryPlacement_VolunteerId",
+                table: "TemporaryPlacement",
+                column: "VolunteerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleUserId",
+                table: "User",
+                column: "RoleUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Volunteers_UserId",
+                table: "Volunteers",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adoptions");
+
+            migrationBuilder.DropTable(
+                name: "TemporaryPlacement");
+
+            migrationBuilder.DropTable(
+                name: "AdoptionalApplications");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Volunteers");
+
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "AppStatuses");
+
+            migrationBuilder.DropTable(
+                name: "EmployeePosts");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "AnimalBreeds");
+
+            migrationBuilder.DropTable(
+                name: "AnimalStatuses");
+
+            migrationBuilder.DropTable(
+                name: "RoleUsers");
+
+            migrationBuilder.DropTable(
+                name: "AnimalViews");
+        }
+    }
+}
